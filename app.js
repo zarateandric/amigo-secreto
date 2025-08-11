4a224dd19865d54cc0ab1afcec8d4d3dd57a2d82
Andric Zarate <zarateandric@gmail.com>
2025-08-11 13:42:12 -0600

    Actualizar index.listaAmigos

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+let nombreAmigo;
+var lista=[];
+
 
 function agregarAmigo(){
-    let nombreAmigo = (document.getElementById("amigo").value);
+    nombreAmigo = (document.getElementById("amigo").value);
     console.log(nombreAmigo);
 
     if(nombreAmigo === ""){
@@ -10,7 +13,9 @@ function agregarAmigo(){
     } else{
         amigos.push(nombreAmigo);
         console.log(amigos);
+        // imprimir nombres de amigos (pendiente)
         limpiarCaja();
+        actualizarListaAmigos();
     }
 
 }
@@ -20,5 +25,24 @@ function limpiarCaja(){
 }
 
 function sortearAmigo(){
-    let amigoSorteado = 
+    let indiceAmigo = Math.floor((Math.random()*amigos.length)+1);
+    console.log(amigoSorteado);
+    if(indiceAmigo === amigos[i]){
+
+    }
+}
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return
+}
+function actualizarListaAmigos(){
+    lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(i=0;i<amigos.length;i++){
+        const li = document.createElement("li");
+        li.textContent = amigos[i]
+        lista.appendChild(li)
+    }
 }
