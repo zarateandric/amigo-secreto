1853a96d26f2a9ca36ff5d60a941f5bd0f51de9d
Andric Zarate <zarateandric@gmail.com>
2025-08-10 19:24:43 -0600

    actualizar app

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo(){
+    let nombreAmigo = (document.getElementById("amigo").value);
+    console.log(nombreAmigo);
+
+    if(nombreAmigo === ""){
+        alert("Por favor, inserte un nombre");
+    } else{
+        amigos.push(nombreAmigo);
+        console.log(amigos);
+        limpiarCaja();
+    }
+
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value ="";
+}
+
+function sortearAmigo(){
+    let amigoSorteado = 
+}
